27a821b25f846ad473aa3458bf33ac8514480525
Rokoos <marcin.widomski83@gmail.com>
2022-10-30 05:25:30 +0100

    Adding Procfile to server

--- index.js
+++ index.js
@@ -57,10 +57,10 @@ app.use(function (err, req, res, next) {
 //     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
 // })
 //for Heroku!!
-app.use(express.static('../client/build'))
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-})
+// app.use(express.static('../client/build'))
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+// })
 
 const port = process.env.PORT || 5000
 
